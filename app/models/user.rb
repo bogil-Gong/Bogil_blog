a6f4d08030f70dd734edfae7ccaa8ce6d1332436
bogil Gong <bogil2006@gmail.com>
2016-06-27 09:56:55 +0000

    비밀번호 백엔드 구축

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,5 @@ class User < ActiveRecord::Base
     validates :email, presence: true, length: { maximum: 105 },
                uniqueness: { case_sensitive: false },
                format: { with: VARID_EMAIL_REGEX }
+    has_secure_password
 end
