900b769d8b5bce2719b1f75d91f97ca37bf7302d
bogil_gong <bogil2006@gmail.com>
2016-06-29 14:20:57 +0000

    로그인 로그아웃 구현

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,10 +5,20 @@ class SessionsController < ApplicationController
     end
     
     def create
-        render 'new'
+        user = User.find_by(email: params[:session][:email].downcase)
+        if user && user.authenticate(params[:session][:password])
+          session[:user_id] = user.id
+          flash[:success] = "로그인 되었습니다."
+          redirect_to user_path(user)
+        else
+          flash.now[:danger] = "잘못된 정보를 입력하셨습니다."
+          render 'new'
+        end
     end
     
     def destroy
-    
+        session[:user_id] = nil
+        flash[:success] = "로그아웃되었습니다"
+        redirect_to root_path
     end
 end
