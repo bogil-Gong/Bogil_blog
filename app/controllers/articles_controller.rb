42d33c92f46e5e3e2c03379919ce91e4f0438e38
bogil_gong <bogil2006@gmail.com>
2016-07-02 10:22:03 +0000

    관리자 권한

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -57,7 +57,7 @@ class ArticlesController < ApplicationController
     end
     
     def require_same_user
-        if current_user != @article.user
+        if current_user != @article.user and !current_user.admin?
             flash[:danger] = "권한이 없습니다"
             redirect_to root_path
         end
