82bb6df01fce214ddb47c179f125dcdfe4aba521
bogil <bogil2006@gmail.com>
2016-07-03 23:58:25 +0900

    관리자가 카테고리를 만들수 있는 권한 설정 및 테스트환경 구축

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,10 @@ class User < ActiveRecord::Base
     before_save { self.email = email.downcase }
     validates :username, presence: true,
                uniqueness: { case_sensitive: false },
-               length: { minimum: 5, maximum: 25}
-   
+               length: { minimum: 3, maximum: 25}
+
     VARID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-    
+
     validates :email, presence: true, length: { maximum: 105 },
                uniqueness: { case_sensitive: false },
                format: { with: VARID_EMAIL_REGEX }
--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -3,6 +3,7 @@ require 'test_helper'
 class CategoriesControllerTest < ActionController::TestCase
   def setup
     @category = Category.create(name: "sports")
+    @user = User.create(username: "john", email: "bogil2006@gmail.com", password: "1111", admin: true)
   end
 
   test "should get categories index" do
@@ -11,12 +12,20 @@ class CategoriesControllerTest < ActionController::TestCase
   end
 
   test "should get new" do
+    session[:user_id] = @user.id
     get :new
     assert_response :success
   end
 
   test "should get show" do
-    get(:show, {'id' => @category.id}) 
+    get(:show, {'id' => @category.id})
     assert_response :success
+  end
+
+  test "should redirect create when admin logged in" do
+    assert_no_difference 'Category.count' do
+      post :create, category: {name: "sports"}
+    end
+    assert_redirected_to categories_path
   end
 end
