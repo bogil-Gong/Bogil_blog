f9440a8d1df1a14e28932ac7f55e13183ff9180c
bogil <bogil2006@gmail.com>
2016-07-05 13:54:46 +0900

    카테고리뷰 업데이트

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,29 +2,29 @@ class ArticlesController < ApplicationController
     before_action :set_article, only: [:edit, :update, :show, :destroy]
     before_action :require_user, except: [:index, :show]
     before_action :require_same_user, only: [:edit, :update, :destroy]
-    
+
     def index
         @articles = Article.paginate(page: params[:page], per_page: 5)
     end
-    
+
     def new
         @article = Article.new
     end
-    
+
     def edit
 
     end
-    
+
     def update
-        
+
         if @article.update(article_params)
             flash[:notice] = "Article was successfully updated"
             redirect_to article_path(@article)
         else
-            render 'edit'            
+            render 'edit'
         end
     end
-    
+
     def create
         @article = Article.new(article_params)
         @article.user = current_user
@@ -35,31 +35,31 @@ class ArticlesController < ApplicationController
             render 'new'
         end
     end
-    
+
     def show
-        
+
     end
-    
+
     def destroy
         @article.destroy
         flash[:danger] = "Article was successfully destroy"
-        
+
         redirect_to articles_path
     end
-    
+
     private
     def set_article
         @article  = Article.find(params[:id])
     end
 
     def article_params
-        params.require(:article).permit(:title, :description)
+        params.require(:article).permit(:title, :description, category_ids: [])
     end
-    
+
     def require_same_user
         if current_user != @article.user and !current_user.admin?
             flash[:danger] = "권한이 없습니다"
             redirect_to root_path
         end
     end
-end 
+end
