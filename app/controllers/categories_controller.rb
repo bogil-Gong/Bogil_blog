3513caa4dd447edaf17b74083c96783d716a40b6
bogil <bogil2006@gmail.com>
2016-07-06 00:39:46 +0900

    카테고리 관리자 계정의 수정기능 구축

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -9,6 +9,20 @@ class CategoriesController < ApplicationController
     @category = Category.new
   end
 
+  def edit
+    @category = Category.find(params[:id])
+  end
+
+  def update
+    @category = Category.find(params[:id])
+    if @category.update(category_params)
+      flash[:success] = "카테고리가 변경되었습니다"
+      redirect_to category_path(@category)
+    else
+      reder 'edit'
+    end
+  end
+
   def show
     @category = Category.find(params[:id])
     @category_articles = @category.articles.paginate(page: params[:page], per_page: 5)
